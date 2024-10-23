f52bf877372e912f1118c05b3bc0bde53b49be3c
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-10-23 12:00:27 -0300

    Create server.cpp

---
+++ server.cpp
@@ -0,0 +1,17 @@
+//server6b.cpp – rodar no Raspberry
+//testa sendBytes e receiveBytes
+#include "projeto.hpp"
+int main(void) {
+ SERVER server;
+ server.waitConnection();
+
+ const int n=100000;
+ BYTE buf[n];
+ memset(buf,111,n); //insere 111 em n bytes a partir do endereço buf
+ server.sendBytes(n,buf);
+ server.receiveBytes(n,buf);
+ if (testaBytes(buf,214,n)) printf("Recebeu corretamente %d bytes %d\n",n,214);
+ else printf("Erro na recepcao de %d bytes %d\n",n,214);
+ memset(buf,111,n);
+ server.sendBytes(n,buf);
+}
