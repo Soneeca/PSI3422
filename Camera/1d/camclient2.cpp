31ab1e94dda81b0687559a383489b2d1d7f00a70
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-10-30 11:01:00 -0300

    Create camclient2.cpp

---
+++ Camera/1d/camclient2.cpp
@@ -0,0 +1,34 @@
+// camclient2.cpp
+#include "projeto.hpp"
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Uso: camclient2 <IP do servidor>" << endl;
+        return 1;
+    }
+
+    CLIENT client(argv[1]);
+
+    Mat frame;
+    namedWindow("Recebendo Quadro", WINDOW_AUTOSIZE);
+    char confirm = '0';
+    while (true) {
+        client.receiveImgComp(frame);
+        if (frame.empty()) break;
+
+        imshow("Recebendo Quadro", frame);
+        int ch = waitKey(30);
+        if (ch == 27) {  // Encerra ao pressionar ESC
+            confirm = 's';
+            client.sendBytes(1, reinterpret_cast<BYTE*>(&confirm));
+            break;
+        }
+
+        client.sendBytes(1, reinterpret_cast<BYTE*>(&confirm));
+    }
+    destroyAllWindows();
+    return 0;
+}
