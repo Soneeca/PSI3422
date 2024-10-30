d2311d276edbee4057a4fdd54cae6cd7f16a97e5
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-10-30 00:37:24 -0300

    Create camserver1.cpp

---
+++ camserver1.cpp
@@ -0,0 +1,32 @@
+// camserver1.cpp
+#include "projeto.hpp"
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main() {
+    SERVER server;
+    server.waitConnection();
+
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cerr << "Erro ao abrir a câmera." << endl;
+        return 1;
+    }
+
+    cap.set(CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+
+    Mat_<Vec3b> frame;
+    char confirm;
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) break;
+
+        server.sendImg(frame);
+
+        server.receiveBytes(1, reinterpret_cast<BYTE*>(&confirm));
+        if (confirm == 's') break;  // Encerra o servidor ao receber 's'
+    }
+    return 0;
+}
