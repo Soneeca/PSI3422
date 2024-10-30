4614b61f0bc5cbf6703548114515c12852843406
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-10-30 11:34:16 -0300

    Update camclient2.cpp

--- Camera/1d/camclient2.cpp
+++ Camera/1d/camclient2.cpp
@@ -1,8 +1,15 @@
-// camclient2.cpp
 #include "projeto.hpp"
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <chrono>
+
 using namespace std;
 using namespace cv;
+
+double timeSinceEpoch() {
+    return chrono::duration_cast<chrono::milliseconds>(
+               chrono::system_clock::now().time_since_epoch()).count() / 1000.0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -14,21 +21,26 @@ int main(int argc, char *argv[]) {
 
     Mat frame;
     namedWindow("Recebendo Quadro", WINDOW_AUTOSIZE);
+
+    double t1 = timeSinceEpoch();
+    int ch = -1;
+    int i = 0; // Contador de quadros recebidos
     char confirm = '0';
-    while (true) {
+
+    while (ch < 0) {
         client.receiveImgComp(frame);
         if (frame.empty()) break;
 
         imshow("Recebendo Quadro", frame);
-        int ch = waitKey(30);
-        if (ch == 27) {  // Encerra ao pressionar ESC
-            confirm = 's';
-            client.sendBytes(1, reinterpret_cast<BYTE*>(&confirm));
-            break;
-        }
-
+        ch = waitKey(1);
+        i++;  // Incrementa o contador de quadros
         client.sendBytes(1, reinterpret_cast<BYTE*>(&confirm));
     }
+
+    double t2 = timeSinceEpoch();
+    double t = t2 - t1;
+    printf("Quadros=%d tempo=%8.2fs fps=%8.2f\n", i, t, i / t);
+
     destroyAllWindows();
     return 0;
 }
