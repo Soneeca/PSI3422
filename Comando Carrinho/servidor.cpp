d4e6d1fa62b7d9b6fecb28e27b4214ff2232da73
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-11-06 10:11:35 -0300

    Update servidor.cpp

--- Comando Carrinho/servidor.cpp
+++ Comando Carrinho/servidor.cpp
@@ -7,109 +7,10 @@
 using namespace std;
 using namespace cv;
 
-void setupMotores() {
-    wiringPiSetup();
-    pinMode(0, OUTPUT);
-    pinMode(1, OUTPUT);
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    digitalWrite(0, LOW);
-    digitalWrite(1, LOW);
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    if (softPwmCreate(0, 0, 100)) exit(1);
-    if (softPwmCreate(1, 0, 100)) exit(1);
-    if (softPwmCreate(2, 0, 100)) exit(1);
-    if (softPwmCreate(3, 0, 100)) exit(1);
-}
-
-void controlarMotores(char comando) {
-    switch (comando) {
-        case '1': // Diagonal Cima Esquerda
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            delay(100);
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '2': // Frente
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '3': // Diagonal Cima Direita
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            delay(100);
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '4': // Rotação Esquerda
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '5': // Freio
-            softPwmWrite(0, 100);
-            softPwmWrite(1, 100);
-            softPwmWrite(2, 100);
-            softPwmWrite(3, 100);
-            break;
-        case '6': // Rotação Direita
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        case '7': // Diagonal Baixo Esquerda
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            delay(100);
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        case '8': // Trás
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        case '9': // Diagonal Baixo Direita
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            delay(100);
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        default: 
-            softPwmStop(0);
-            softPwmStop(1);
-            softPwmStop(2);
-            softPwmStop(3);
-            break;
-    }
-}
+// Funções de controle dos motores (setupMotores e controlarMotores) continuam aqui...
 
 int main(int argc, char** argv) {
-    SERVER server; // Ajustado para usar o construtor padrão
+    SERVER server; // Alterado para usar o construtor padrão
 
     server.waitConnection();
     setupMotores();
@@ -124,7 +25,7 @@ int main(int argc, char** argv) {
     cap.set(CAP_PROP_FRAME_HEIGHT, 240);
 
     Mat_<COR> frame;
-    char comando = '5';  // Inicializando com o comando '5' para o freio
+    char comando;
 
     while (true) {
         server.receiveBytes(1, reinterpret_cast<BYTE*>(&comando)); // Modificado para remover `<= 0`
