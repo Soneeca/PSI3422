c2adfd1004af37e7e3145ff275635fbf58a70251
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-11-06 11:25:19 -0300

    Update servidor.cpp

--- Comando Carrinho/servidor.cpp
+++ Comando Carrinho/servidor.cpp
@@ -1,105 +1,76 @@
-// Stephanie Lonzhu Chi - 11257251
-// Vinicius de Barros Silva - 10335913
-//servidor2.cpp
-
 #include "projeto.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
 #include <wiringPi.h>
 #include <softPwm.h>
 
-int main (void) {
-	SERVER server;
-	server.waitConnection();
-	
-	VideoCapture w(0);
-	Mat_<COR> a;
-	uint32_t client_ready;
-	uint32_t state;
-	uint32_t close;
+using namespace std;
+using namespace cv;
 
-	wiringPiSetup();
-	if (softPwmCreate(0, 0, 100)) erro("ERROR: Soft PWM Create");
-	if (softPwmCreate(1, 0, 100)) erro("ERROR: Soft PWM Create");
-	if (softPwmCreate(2, 0, 100)) erro("ERROR: Soft PWM Create");
-	if (softPwmCreate(3, 0, 100)) erro("ERROR: Soft PWM Create");
-	
-	if (!w.isOpened()) erro("ERROR: SERVIDOR 2: Webcam");
-	w.set(CV_CAP_PROP_FRAME_WIDTH, 320);
-    w.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
-    
-    while(true) {
-		server.receiveUint(client_ready);
-		server.receiveUint(state);
-		if (state == 0) {
-			w >> a;
-			putTxt(a, 5, 5, "0", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(0, 0); 
-			softPwmWrite(1, 0); 
-			softPwmWrite(2, 0); 
-			softPwmWrite(3, 0); 
-		}
-		if (state == 1) {
-			w >> a;
-			putTxt(a, 5, 5, "1", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(1, 0); 
-			softPwmWrite(3, 100);
-		}
-		if (state == 2) {
-			w >> a;
-			putTxt(a, 5, 5, "2", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(1, 100); 
-			softPwmWrite(3, 80);
-		}
-		if (state == 3) {
-			w >> a;
-			putTxt(a, 5, 5, "3", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(1, 100); 
-			softPwmWrite(3, 0);
-		}
-		if (state == 4) {
-			w >> a;
-			putTxt(a, 5, 5, "4", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(1, 100); 
-			softPwmWrite(2, 100);
-		}
-		if (state == 6) {
-			w >> a;
-			putTxt(a, 5, 5, "6", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(0, 100); 
-			softPwmWrite(3, 100);
-		}
-		if (state == 7) {
-			w >> a;
-			putTxt(a, 5, 5, "7", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(2, 100); 
-			softPwmWrite(3, 0); 
-		}
-		if (state == 8) {
-			w >> a;
-			putTxt(a, 5, 5, "8", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(0, 100); 
-			softPwmWrite(2, 80);
-		}
-		if (state == 9) {
-			w >> a;
-			putTxt(a, 5, 5, "9", COR(0, 0, 255), 5);
-			server.sendImgComp(a);
-			softPwmWrite(0, 100); 
-			softPwmWrite(1, 0);
-		}
-		
-		server.receiveUint(close);
-		if (close == 1) {
-			cout << "SERVIDOR 2: Closing" << endl;
-			break;
-		}
-	}
+void setupMotores() {
+    wiringPiSetup();
+    pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    digitalWrite(0, LOW);
+    digitalWrite(1, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    if (softPwmCreate(0, 0, 100)) exit(1);
+    if (softPwmCreate(1, 0, 100)) exit(1);
+    if (softPwmCreate(2, 0, 100)) exit(1);
+    if (softPwmCreate(3, 0, 100)) exit(1);
+}
 
+void controlarMotores(uint32_t comando) {
+    switch (comando) {
+        case 1: cout << "Diagonal Cima Esquerda\n"; break;
+        case 2: cout << "Frente\n"; break;
+        case 3: cout << "Diagonal Cima Direita\n"; break;
+        case 4: cout << "Rotação Esquerda\n"; break;
+        case 5: cout << "Freio\n"; break;
+        case 6: cout << "Rotação Direita\n"; break;
+        case 7: cout << "Diagonal Baixo Esquerda\n"; break;
+        case 8: cout << "Trás\n"; break;
+        case 9: cout << "Diagonal Baixo Direita\n"; break;
+        default: cout << "Parado\n"; break;
+    }
+}
+
+int main() {
+    SERVER server;
+    server.waitConnection();
+    setupMotores();
+
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cerr << "Erro ao abrir a câmera.\n";
+        return 1;
+    }
+
+    cap.set(CAP_PROP_FRAME_WIDTH, 320);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 240);
+
+    Mat_<COR> frame;
+    uint32_t comando = 5, ultimo_comando = 0;
+
+    while (true) {
+        server.receiveUint(comando);
+        if (comando != ultimo_comando) {
+            cout << "Comando recebido: " << comando << endl;
+            controlarMotores(comando);
+            ultimo_comando = comando;
+        }
+
+        cap >> frame;
+        if (frame.empty()) break;
+
+        server.sendImgComp(frame);
+        server.receiveUint(comando);
+        if (comando == 27) break;
+    }
+
+    cout << "Servidor finalizado.\n";
+    return 0;
 }
