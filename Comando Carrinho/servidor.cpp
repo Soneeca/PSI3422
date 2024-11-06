3e87e6c985640d3e5080cc4c5b758997849f58fe
Pedro Ian de Almeida Soares <62733610+Soneeca@users.noreply.github.com>
2024-11-06 12:20:13 -0300

    Update servidor.cpp

--- Comando Carrinho/servidor.cpp
+++ Comando Carrinho/servidor.cpp
@@ -1,151 +1,37 @@
 #include "projeto.hpp"
-#include "raspberry.hpp"
-#include <opencv2/opencv.hpp>
 #include <iostream>
-#include <wiringPi.h>
-#include <softPwm.h>
 
 using namespace std;
-using namespace cv;
 
-void setupMotores() {
-    wiringPiSetup();
-    pinMode(0, OUTPUT);
-    pinMode(1, OUTPUT);
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    digitalWrite(0, LOW);
-    digitalWrite(1, LOW);
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    if (softPwmCreate(0, 0, 100)) exit(1);
-    if (softPwmCreate(1, 0, 100)) exit(1);
-    if (softPwmCreate(2, 0, 100)) exit(1);
-    if (softPwmCreate(3, 0, 100)) exit(1);
-}
-
-void controlarMotores(char comando) {
+void controlaMotor(uint32_t comando) {
+    // Função para controlar o motor com base no comando recebido
     switch (comando) {
-        case '1': // Diagonal Cima Esquerda
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            delay(100);
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '2': // Frente
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '3': // Diagonal Cima Direita
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            delay(100);
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '4': // Rotação Esquerda
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            break;
-        case '5': // Freio
-            softPwmWrite(0, 100);
-            softPwmWrite(1, 100);
-            softPwmWrite(2, 100);
-            softPwmWrite(3, 100);
-            break;
-        case '6': // Rotação Direita
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        case '7': // Diagonal Baixo Esquerda
-            softPwmWrite(0, 50);
-            softPwmWrite(1, 0);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            delay(100);
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        case '8': // Trás
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        case '9': // Diagonal Baixo Direita
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 50);
-            softPwmWrite(3, 0);
-            delay(100);
-            softPwmWrite(0, 0);
-            softPwmWrite(1, 50);
-            softPwmWrite(2, 0);
-            softPwmWrite(3, 50);
-            break;
-        default: 
-            softPwmStop(0);
-            softPwmStop(1);
-            softPwmStop(2);
-            softPwmStop(3);
-            break;
+        case 1: cout << "Motor: Esquerda Cima" << endl; break;
+        case 2: cout << "Motor: Cima" << endl; break;
+        case 3: cout << "Motor: Direita Cima" << endl; break;
+        case 4: cout << "Motor: Esquerda" << endl; break;
+        case 5: cout << "Motor: Parado" << endl; break;
+        case 6: cout << "Motor: Direita" << endl; break;
+        case 7: cout << "Motor: Esquerda Baixo" << endl; break;
+        case 8: cout << "Motor: Baixo" << endl; break;
+        case 9: cout << "Motor: Direita Baixo" << endl; break;
+        case 0: cout << "Motor: Parado (Sinal de Encerramento)" << endl; break;
+        default: cout << "Comando desconhecido!" << endl; break;
     }
 }
 
-int main(int argc, char** argv) {
-    SERVER server; // Usa o construtor padrão
-
+int main() {
+    SERVER server;
     server.waitConnection();
-    setupMotores();
-
-    VideoCapture cap(0);
-    if (!cap.isOpened()) {
-        cerr << "Erro ao abrir a câmera." << endl;
-        return 1;
-    }
-
-    cap.set(CAP_PROP_FRAME_WIDTH, 320);
-    cap.set(CAP_PROP_FRAME_HEIGHT, 240);
-
-    Mat_<COR> frame;
-    char comando = '5';  // Comando inicial para o freio
-    char ultimo_comando = '0';  // Armazena o último comando para verificar mudanças
+    uint32_t comando;
 
     while (true) {
-        server.receiveBytes(1, reinterpret_cast<BYTE*>(&comando));
-        if (comando == 's') break;
+        server.receiveUint(comando);  // Recebe o comando como uint32_t
 
-        // Se o comando mudou, imprime e atualiza o último comando
-        if (comando != ultimo_comando) {
-            cout << "Novo comando recebido: " << comando << endl;
-            ultimo_comando = comando;
-        }
-
-        controlarMotores(comando);
-
-        cap >> frame;
-        if (frame.empty()) break;
-
-        server.sendImgComp(frame);
+        if (comando == 0) break;  // Sinal de parada
+        controlaMotor(comando);   // Chama a função para mover o motor
     }
 
-    cout << "Servidor finalizado." << endl;
+    cout << "Conexão encerrada pelo cliente." << endl;
     return 0;
 }
